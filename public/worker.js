c0e2e076d157ca3f7b11c55fb2311b0cec4c5eab
Aditya Dhade <76063440+b1ink0@users.noreply.github.com>
2023-02-09 19:12:47 +0530

    pwa_update

---
+++ public/worker.js
@@ -0,0 +1,55 @@
+var CACHE_NAME = "ShareVoid";
+var urlsToCache = [
+    "/", 
+    "favicon.svg",
+    "logo192.png",
+    "logo512.png",
+    "manifest.json",
+    "index.html",
+    "maskable128.png",
+    "maskable192.png",
+    "maskable384.png",
+    "maskable512.png",
+    "worker.js",
+    
+];
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = ["ShareVoid"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
